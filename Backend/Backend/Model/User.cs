af389ac5f6754d134124b9a72c321aa4e11bef7f
kartikey-gour <kartikey.gour.cdac@gmail.com>
2026-02-18 08:22:21 +0530

    user entity

--- Backend/Backend/Model/User.cs
+++ Backend/Backend/Model/User.cs
@@ -2,5 +2,11 @@
 {
     public class User
     {
+        int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string HashedPassword { get; set; }
+        public string CreatedAt { get; set; }
+        
     }
 }
