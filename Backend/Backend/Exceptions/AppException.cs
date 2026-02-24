00b0540d877f1ce3e528c47866569bb526f7dde8
Kartikey Gour <kartikey.gour.cdac@gmail.com>
2026-02-24 18:30:21 +0530

    Added : AppException & updated InvalidCredentialException

---
+++ Backend/Backend/Exceptions/AppException.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Net;
+
+namespace Backend.Exceptions
+{
+    public class AppException : Exception
+    {
+        public HttpStatusCode StatusCode { get; };      //Middleware decides HTTP response using this property
+        public string ErrorCode { get; }  = string.Empty;
+
+        protected AppException(HttpStatusCode statusCode, string errorCode, string message) : base(message)
+        {
+            StatusCode = statusCode;
+            ErrorCode = errorCode;
+        }
+    }
+}
