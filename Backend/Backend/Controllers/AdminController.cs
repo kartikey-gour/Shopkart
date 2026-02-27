9a400757ecc8bc739b07eca724f936267aa7e1a4
Kartikey Gour <kartikey.gour.cdac@gmail.com>
2026-02-27 17:58:50 +0530

    Tested user registraion, and done AuthController

---
+++ Backend/Backend/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("shopkart/admin")]
+    public class AdminController : ControllerBase
+    {
+        [Authorize(Roles = "Admin")]
+        [HttpGet("dashboard")]
+        public IActionResult GetDashboard()
+        {
+            return Ok(
+                new
+                {
+                    message = "Welcome Admin",
+                    user = User.Identity?.Name
+                }
+            );
+        }
+    }
+}
---
+++ Backend/Backend/Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using Backend.DTOs;
+using Backend.Services.Implementations;
+using Backend.Services.Interfaces;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("shopkart/auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        //ctor based DI
+        public AuthController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<AuthResponseDto>> Register(RegisterRequestDto dto)
+        {
+            var result = await _userService.RegisterAsync(dto);
+
+            return Ok(result);
+        }
+
+        [HttpPost("Login")]
+        public async Task<ActionResult<AuthResponseDto>> Login(LoginRequestDto dto)
+        {
+            var result = await _userService.LoginAsync(dto);
+
+            return Ok(result);
+        }
+    }
+}
